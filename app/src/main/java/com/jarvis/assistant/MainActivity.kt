86463b92dd1337f56308c0eabab1a3ec4e922aa7
aivideo66678-cloud <aivideo66678@gmail.com>
2026-07-29 09:15:16 +0545

    Create MainActivity with greeting message

---
+++ app/src/main/java/com/jarvis/assistant/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.jarvis.assistant
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "Hello, I am Jarvis!"
+        textView.textSize = 24f
+        setContentView(textView)
+    }
+}
